c8b2b9c1acb059dcc77a1916554ba47237632a4f
Guy Langston <guylangston@gmail.com>
2020-04-22 07:56:57 +0100

    nuget

---
+++ TextRenderZ/publish.ps1
@@ -0,0 +1,28 @@
+param(
+     [string]$ver
+    )
+$name = "TextRenderZ"
+
+if ([string]::IsNullOrEmpty($ver))
+{
+    $ver = Get-Content ./package-version.txt
+}
+
+echo "Package: '$name'"
+echo "Package-Version: '$ver'"
+
+$confirmation = Read-Host "Does the version look correct? y/n"
+if ($confirmation -eq 'y') {
+    pushd
+    
+    dotnet build -c Release --no-incremental "-p:PackageVersion=$ver"
+    dotnet pack -c Release "-p:PackageVersion=$ver"
+    
+    
+    copy ".\bin\Release\$name.$ver.nupkg"  "C:\Projects\LocalNuGet\$name.$ver.nupkg"
+    
+    echo " ==> C:\Projects\LocalNuGet\$name.$ver.nupkg"
+    
+    popd
+}
+
